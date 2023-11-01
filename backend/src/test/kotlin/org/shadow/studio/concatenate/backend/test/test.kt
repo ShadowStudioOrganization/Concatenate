0ce0fe40562fc6f66aaf17339c0df09c81dcd3ed
whiterasbk <whiterasbk@qq.com>
2023-11-01 18:07:20 +0800

    opt tests

--- backend/src/test/kotlin/org/shadow/studio/concatenate/backend/test/test.kt
+++ backend/src/test/kotlin/org/shadow/studio/concatenate/backend/test/test.kt
@@ -2,18 +2,15 @@ package org.shadow.studio.concatenate.backend.test
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import org.shadow.studio.concatenate.backend.util.JsonUtilScope
 import org.shadow.studio.concatenate.backend.util.mappingGameArguments
-import org.shadow.studio.concatenate.backend.util.parseGameArguments
 
 fun main() {
 
     val json = getResourceAsString("flandrebakapack-1.20.1.json")
     val mapper = jacksonObjectMapper()
     val map: Map<String, Any> = mapper.readValue(json)
-    val k = mappingGameArguments(map, mapOf("auth_player_name" to "whiterasbk"))
+    val k = mappingGameArguments(map, mapOf("auth_player_name" to "whiterasbk", "quickPlayPath" to "1"))
     println(k.joinToString(" "))
-
 
 
 }
