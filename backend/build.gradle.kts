9580d6c0bd8c95142d990e780fcaf12a8eff4518
whiterasbk <whiterasbk@qq.com>
2023-11-02 23:48:19 +0800

    add maven resolver deps

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -9,6 +9,15 @@ dependencies {
     implementation("io.ktor:ktor-client-core:2.3.5")
     implementation("io.ktor:ktor-client-cio:2.3.5")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+
+    implementation("org.apache.maven.resolver:maven-resolver-api:1.9.16")
+    implementation("org.apache.maven:maven-resolver-provider:3.9.5")
+    implementation("org.apache.maven.resolver:maven-resolver-connector-basic:1.9.16")
+    implementation("org.apache.maven.resolver:maven-resolver-transport-http:1.9.16")
+    implementation("org.apache.maven.resolver:maven-resolver-impl:1.9.16")
+
+
+
 }
 
 tasks.test {
